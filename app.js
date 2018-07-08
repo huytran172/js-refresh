888f8712fc23a3372044966ba99e3028b6214214
Huy Tran <htran@immunetolerance.org>
2018-07-08 15:40:31 -0400

    init repo

--- app.js
+++ app.js
@@ -157,4 +157,160 @@ const reverseArrayInPlace = arr => {
 console.log(reverseArray([1, 2, 3, 4, 5, 6]));
 console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6]));
 
-// Exercise 11 A list
+// Reduce function
+console.log([1, 2, 3, 4].reduce((total, current) => total + current));
+
+// methods
+function speak(line) {
+    console.log('What is this ' + line + ' ' + this.type);
+}
+
+let rabbit = { type: 'unknown', speak };
+
+rabbit.speak('Hello world');
+speak.call(rabbit, 'burp');
+
+function Rabbit(type) {
+    this.type = type;
+}
+
+Rabbit.prototype.speak = function (line) {
+    console.log(`${this.type} said ${line}`);
+}
+
+console.log((new Rabbit('WTF').speak('hello')));
+
+let okIterator = 'OK'[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
+
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+}
+
+// Vector type
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(anotherVector) {
+        const x = this.x + anotherVector.x;
+        const y = this.y + anotherVector.y;
+        return new Vec(x, y);
+    }
+
+
+    minus(anotherVector) {
+        const x = this.x - anotherVector.x;
+        const y = this.y - anotherVector.y;
+        return new Vec(x, y);
+    }
+
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+
+const vecA = new Vec(2, 3);
+const vecB = new Vec(5, 2);
+console.log(vecA.plus(vecB));
+console.log(vecA.length);
+
+
+// Groups
+class Group {
+    constructor() {
+        // this.array = array.filter((value, index, self) => {
+        //     return self.indexOf(value) === index
+        // });
+
+        this.array = [];
+    }
+
+    add(value) {
+        if (! this.has(value)) {
+            this.array.push(value);
+        } 
+    }
+
+    delete(value) {
+        this.array = this.array.filter(v => v !== value);
+    }
+
+    has(value) {
+        return this.array.includes(value);
+    }
+
+    get length() {
+        return this.array.length;
+    }
+
+    get(index) {
+        return this.array[index];
+    }
+
+    static from(collection) {
+        let group = new Group();
+        for (let item of collection) {
+            group.add(item);
+        }
+
+        return group;
+    }
+
+    [Symbol.iterator]() {
+        return new GroupIterator(this);
+    }
+}
+
+class GroupIterator {
+    constructor(group) {
+        this.index = 0;
+        this.group = group;
+    }
+
+    next() {
+        if (this.index >= this.group.length) return { done: true };
+
+        let value = {
+            index: this.index,
+            value: this.group.get(this.index),
+            done: false
+        }
+
+        this.index++;
+
+        return value;
+    }
+}
+
+
+let group = Group.from([1, 2, 3, 4, 4, 10]);
+console.log(group);
+
+for (let item of group) {
+    console.log(item)
+}
+
+
